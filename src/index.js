f45224856298dedeb9e8eba60868da256b986185
AugustineCervantes <Augustine.CervantesIII@gmail.com>
2020-05-27 14:26:16 -0500

    adding changes

--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ const $ = require('jquery');
 const sayHello = 'hello';
 console.log(sayHello);
 
-$('body').css('background-color', 'green');
+$('body').css('background-color', 'pink');
+
+//have to 'npm run build' in terminal when a change is made
